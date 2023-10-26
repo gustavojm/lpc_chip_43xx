033edb9d297594195d3dd0f84327dd018a990667
Gustavo Malano <gmalano@na-sa.com.ar>
2023-10-26 10:11:30 -0300

    initial commit

---
+++ Debug/src/fpu_init.d
@@ -0,0 +1,17 @@
+src/fpu_init.o src/fpu_init.d: ../src/fpu_init.c \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/config_43xx/sys_config.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/cmsis.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/lpc_types.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/cmsis_43xx.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cm4.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cmInstr.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cmFunc.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cm4_simd.h
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/config_43xx/sys_config.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/cmsis.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/lpc_types.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/cmsis_43xx.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cm4.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cmInstr.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cmFunc.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/core_cm4_simd.h:
---
+++ Debug/src/ring_buffer.d
@@ -0,0 +1,5 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/ring_buffer.h \
+ /usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/lpc_types.h
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/ring_buffer.h:
+/usr/local/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx/inc/lpc_types.h:
